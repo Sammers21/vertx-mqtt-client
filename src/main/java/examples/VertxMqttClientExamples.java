4dd4a8f631525676ad9dffc470b0277d84f13ec1
Pavel Drankov <titantins@gmail.com>
2017-06-23 19:08:45 +0300

    add initial documentation

---
+++ src/main/java/examples/VertxMqttClientExamples.java
@@ -0,0 +1,54 @@
+package examples;
+
+import io.netty.handler.codec.mqtt.MqttQoS;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.mqtt.MqttClient;
+import io.vertx.mqtt.MqttClientOptions;
+import io.vertx.mqtt.impl.MqttClientImpl;
+
+public class VertxMqttClientExamples {
+
+  /**
+   * Example for demonstration of how {@link MqttClient#connect()} and  {@link MqttClient#disconnect()} methods
+   * should be used
+   *
+   * @param vertx
+   */
+  public void example1(Vertx vertx) {
+    MqttClientOptions options = new MqttClientOptions()
+      .setHost("iot.eclipse.org")
+      .setPort(1883);
+
+    MqttClient client = new MqttClientImpl(vertx, options);
+
+    client.connect(s -> client.disconnect());
+  }
+
+  /**
+   * Example for handling publish messages from server
+   *
+   * @param client
+   */
+  public void example2(MqttClient client) {
+    client.publishHandler(s -> {
+      System.out.println("There are new message in topic: " + s.topicName());
+      System.out.println("Content(as string) of the message: " + s.payload().toString());
+      System.out.println("QoS: " + s.qosLevel());
+    })
+      .subscribe("rpi2/temp", 2);
+  }
+
+  /**
+   * Example for sending publish message
+   *
+   * @param client
+   */
+  public void example3(MqttClient client) {
+    client.publish("temperature",
+      Buffer.buffer("hello".getBytes()),
+      MqttQoS.AT_LEAST_ONCE,
+      false,
+      false);
+  }
+}
--- src/main/java/io/vertx/mqtt/package-info.java
+++ src/main/java/io/vertx/mqtt/package-info.java
@@ -1,7 +1,44 @@
 /**
  * = Vert.x MQTT client
  *
- * include::{@link io.vertx.mqtt}[]
+ * == Using Vert.x MQTT client
+ * == Getting started
+ *
+ * === Connect/Disconnect
+ * The client gives you opportunity to connect to a server and disconnect from it.
+ * Also, you could specify things like the host and port of a server you would like
+ * to connect to passing instance of {@link io.vertx.mqtt.MqttClientOptions} as a param through constructor.
+ *
+ * This example shows how you could connect to a server and disconnect from it using Vert.x MQTT client and calling {@link io.vertx.mqtt.MqttClient#connect()} and {@link io.vertx.mqtt.MqttClient#disconnect()} methods.
+ * [source,$lang]
+ * ----
+ * {@link examples.VertxMqttClientExamples#example1}
+ * ----
+ * NOTE: default address of server provided by {@link io.vertx.mqtt.MqttClientOptions} is localhost:1883 and localhost:8883 if you are using SSL/TSL.
+ * === Subscribe to a topic
+ *
+ * Now, lest go deeper and take look at this example:
+ *
+ * [source,$lang]
+ * ----
+ * {@link examples.VertxMqttClientExamples#example2}
+ * ----
+ *
+ * Here we have the example of usage of {@link io.vertx.mqtt.MqttClient#subscribe(java.lang.String, int)} method. In order to receive messages from rpi2/temp topic we call {@link io.vertx.mqtt.MqttClient#subscribe(java.lang.String, int)} method.
+ * Although, to handle received messages from server you need to provide a handler, which will be called each time you have a new messages in the topics you subscribe on.
+ * As this example shows, handler could be provided via {@link io.vertx.mqtt.MqttClient#publishHandler(io.vertx.core.Handler)} method.
+ *
+ * === Publishing message to a topic
+ *
+ * If you would like to publish some message into topic then {@link io.vertx.mqtt.MqttClient#publish(java.lang.String, io.vertx.core.buffer.Buffer, io.netty.handler.codec.mqtt.MqttQoS, boolean, boolean)} should be called.
+ * Let's take a look at the example:
+ * [source,$lang]
+ * ----
+ * {@link examples.VertxMqttClientExamples#example3}
+ * ----
+ *
+ * In the example we send message to topic with name "temperature".
+ *
  */
 @Document(fileName = "index.adoc")
 @ModuleGen(name = "vertx-mqtt-client", groupPackage = "io.vertx")
